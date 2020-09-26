e42e0d6bc474ae2d0681f92bce5ac2ea11900d94
fiona <fionaniwe@gmail.com>
2020-09-26 11:42:55 +0200

    century formular

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ dateOfBirth = document.getElementById("date").value
 gender = document.getElementById("gender").value
 
 dateOfBirth = new Date(dateOfBirth);
+
+var day=parseInt(dateOfBirth.getFullYear());
+var month=parseInt(dateOfBirth.getMonth());
+var year=parseInt(dateOfBirth.getDate());
+
+    //Centuary calculation
+    var centry=(year-1)/100+1; 
