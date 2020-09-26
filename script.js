82d95da5185975a8a9739a4a9e394b59db720e1c
fiona <fionaniwe@gmail.com>
2020-09-26 11:54:18 +0200

    femaleAkanNames array is created

--- script.js
+++ script.js
@@ -16,4 +16,8 @@ var year=parseInt(dateOfBirth.getDate());
     var dayOfTheWeek = ( centry/4 -2*centry-1 + 5*year/4  + 26*(month+1)/10 + day ) % 7;
 
      dayOfTheWeek = parseInt(dayOfTheWeek);  
+     var akanName;
+
+     femaleAkanNames = ["Ama", "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua"];
      
+ 
